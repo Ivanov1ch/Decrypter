c3a27dbe21a7407de224c3aae1d6213c24980db2
jzhu2020 <jzhu2020@k12.andoverma.us>
2018-11-08 11:32:10 -0500

    Add Incomplete Decryption classes and CharSet enum

---
+++ Decryption/Caesar.java
@@ -0,0 +1,53 @@
+/**
+ * File:        Caesar.java
+ * Description: Decrypts Caesar Ciphers.
+ * Created:     11/08/18
+ *
+ * @author Justin Zhu
+ * @version 1.0
+ */
+
+public class Caesar extends Decrypter {
+
+	public Caesar(String cipherText) {
+        super(cipherText);
+    }
+
+	public String decrypt(int shift, CharSet charset) {
+		char base;
+		int numChars;
+		switch(charset) { // TODO Add more cases
+			case CharSet.ALPHABETIC:
+				base = 'a';
+				numChars = 26;
+				break;
+		}
+
+		String plainText = "";
+		for(int i = 0; i < cipherText.length(); i++) {
+			char c = cipherText.charAt(i);
+			boolean whitespace = false;
+			switch (c) {
+				case ' ':
+				case '\n':
+				case '\t':
+					whitespace = true;
+					break;
+			}
+
+			switch(charset) {
+				case CharSet.ALPHABETIC:
+					if(Character.isUpperCase(c)) // TODO
+					break;
+			}
+		}
+	}
+
+	public String decrypt(String key, CharSet charset) {
+		try {
+			return decrypt(Integer.parseInt(key), charset);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}

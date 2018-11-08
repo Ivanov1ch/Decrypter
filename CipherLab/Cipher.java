a0acb7fe4dbde4574ce35042dfe03780d4038195
Daniel Ivanovich <Soccer.Ivanovich@gmail.com>
2018-11-08 09:10:59 -0500

    Reformat code, set up file template

--- CipherLab/Cipher.java
+++ CipherLab/Cipher.java
@@ -11,70 +11,63 @@ import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 
 public class Cipher extends JFrame
-		implements ActionListener
-{
-	private JTextArea original, encrypted, words;
-	private JButton go;
+        implements ActionListener {
+    private JTextArea original, encrypted, words;
+    private JButton go;
 
-	// Constructor
-	public Cipher()
-	{
-		super("Caesar Cipher");
+    // Constructor
+    public Cipher() {
+        super("Caesar Cipher");
 
-		setJMenuBar(new CipherMenu(this));
-		setupGui();
+        setJMenuBar(new CipherMenu(this));
+        setupGui();
 
-		original.setText("Type or paste your text here or load it from a file");
-		refresh();
-	}
+        original.setText("Type or paste your text here or load it from a file");
+        refresh();
+    }
 
-	public String getText()
-	{
-		return original.getText();
-	}
+    public String getText() {
+        return original.getText();
+    }
 
-	public String getEncrypted() {
-		return encrypted.getText();
-	}
+    public String getEncrypted() {
+        return encrypted.getText();
+    }
 
-	public void setText(String text)
-	{
-		original.setText(text);
-	}
+    public void setText(String text) {
+        original.setText(text);
+    }
 
-	public void refresh()
-	{
-		String text = original.getText();
-		Encryptor encryptor = new Encryptor(text);
-		encrypted.setText(encryptor.getEncrypted());
-		//words.setText(encryptor.allWordsWith('e'));
-	}
- 
-	// Called when the Refresh burron is clicked
-	public void actionPerformed(ActionEvent e)
-	{
-		refresh();
-	}
+    public void refresh() {
+        String text = original.getText();
+        Encryptor encryptor = new Encryptor(text);
+        encrypted.setText(encryptor.getEncrypted());
+        //words.setText(encryptor.allWordsWith('e'));
+    }
 
-	// ********************** GUI setup ********************************
+    // Called when the Refresh burron is clicked
+    public void actionPerformed(ActionEvent e) {
+        refresh();
+    }
 
-	private void setupGui()
-	{
-		original = new JTextArea(10, 20);
-		original.setLineWrap(true);
-		original.setWrapStyleWord(true);
-		JScrollPane originalPane = new JScrollPane(original,
-			ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
-			ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+    // ********************** GUI setup ********************************
 
-		encrypted = new JTextArea(10, 20);
-		encrypted.setEditable(false);
-		encrypted.setBackground(Color.LIGHT_GRAY);
-		encrypted.setLineWrap(true);
-		encrypted.setWrapStyleWord(true);
-		JScrollPane encryptedPane = new JScrollPane(encrypted,
-			ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
-			ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+    private void setupGui() {
+        original = new JTextArea(10, 20);
+        original.setLineWrap(true);
+        original.setWrapStyleWord(true);
+        JScrollPane originalPane = new JScrollPane(original,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        encrypted = new JTextArea(10, 20);
+        encrypted.setEditable(false);
+        encrypted.setBackground(Color.LIGHT_GRAY);
+        encrypted.setLineWrap(true);
+        encrypted.setWrapStyleWord(true);
+        JScrollPane encryptedPane = new JScrollPane(encrypted,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
 		/*
 		words = new JTextArea(10, 10);
@@ -84,35 +77,34 @@ public class Cipher extends JFrame
 			ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
 			ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		*/
-		go = new JButton("Refresh");
-		go.addActionListener(this);
+        go = new JButton("Refresh");
+        go.addActionListener(this);
 
-		Box box1 = Box.createVerticalBox();
-		box1.add(Box.createVerticalStrut(10));
-		box1.add(originalPane);
-		box1.add(Box.createVerticalStrut(10));
-		box1.add(encryptedPane);
-		box1.add(Box.createVerticalStrut(10));
-		box1.add(go);
-		//Box box2 = Box.createVerticalBox();
-		//box2.add(Box.createVerticalStrut(10));
-		//box2.add(wordsPane);
-		//box2.add(Box.createVerticalStrut(40));
-		Box box3 = Box.createHorizontalBox();
-		box3.add(box1);
-		box3.add(Box.createHorizontalStrut(20));
-		//box3.add(box2);
+        Box box1 = Box.createVerticalBox();
+        box1.add(Box.createVerticalStrut(10));
+        box1.add(originalPane);
+        box1.add(Box.createVerticalStrut(10));
+        box1.add(encryptedPane);
+        box1.add(Box.createVerticalStrut(10));
+        box1.add(go);
+        //Box box2 = Box.createVerticalBox();
+        //box2.add(Box.createVerticalStrut(10));
+        //box2.add(wordsPane);
+        //box2.add(Box.createVerticalStrut(40));
+        Box box3 = Box.createHorizontalBox();
+        box3.add(box1);
+        box3.add(Box.createHorizontalStrut(20));
+        //box3.add(box2);
 
-		Container c = getContentPane();
-		c.setLayout (new FlowLayout());
-		c.add(box3);
-	}
+        Container c = getContentPane();
+        c.setLayout(new FlowLayout());
+        c.add(box3);
+    }
 
-	public static void main(String[] args)
-	{
-		Cipher window = new Cipher();
-		window.setBounds(100, 100, 480, 480);
-		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		window.setVisible(true);
-	}
+    public static void main(String[] args) {
+        Cipher window = new Cipher();
+        window.setBounds(100, 100, 480, 480);
+        window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
 }
